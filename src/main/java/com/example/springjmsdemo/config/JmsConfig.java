ba5a35c784a0735e55b1ab03b90d07d04a476eeb
jfspps <jfsapps@gmail.com>
2021-02-26 23:59:46 +0000

    built Sender class which sends messages with JmsTemplate

--- src/main/java/com/example/springjmsdemo/config/JmsConfig.java
+++ src/main/java/com/example/springjmsdemo/config/JmsConfig.java
@@ -9,6 +9,8 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
 
+    public static final String MY_QUEUE = "Greetings!";
+
     @Bean
     public MessageConverter messageConverter(){
         // utilise the Jackson JSON library
---
+++ src/main/java/com/example/springjmsdemo/sender/SendersMessage.java
@@ -0,0 +1,36 @@
+package com.example.springjmsdemo.sender;
+
+import com.example.springjmsdemo.config.JmsConfig;
+import com.example.springjmsdemo.model.SimpleMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@AllArgsConstructor
+public class SendersMessage {
+
+    // handles pre-configured ActiveMQ server credentials and establish connection for SendersMessage
+    private final JmsTemplate jmsTemplate;
+
+    // 2000 msec
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage(){
+
+        // confirm SendersMessage is wired in
+        System.out.println("Sending a message");
+
+        // build the payload
+        SimpleMessage message = SimpleMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Howdy!")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+
+        System.out.println("Message sent");
+    }
+}
