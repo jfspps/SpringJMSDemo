c96ed06058a3e77bf66452d1867bf7ebc74125ab
jfspps <jfsapps@gmail.com>
2021-02-26 01:39:32 +0000

    added embedded server with config (note: errors thrown on startup)

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
 # SpringJMSDemo
 
-Spring Boot demo of JMS messaging. This project also uses an embedded server, with the following dependencies at the time of writing:
+Spring Boot demo of JMS messaging. This project also uses an embedded server, with the following dependencies at the
+time of writing:
 
 ```xml
-        <dependency>
-            <groupId>org.apache.activemq</groupId>
-            <artifactId>artemis-server</artifactId>
-            <version>2.17.0</version>
-        </dependency>
-
-<!--        adding an embedded server  -->
-        <dependency>
-            <groupId>org.apache.activemq</groupId>
-            <artifactId>artemis-jms-server</artifactId>
-            <version>2.17.0</version>
-        </dependency>
+<dependency>
+    <groupId>org.apache.activemq</groupId>
+    <artifactId>artemis-server</artifactId>
+</dependency>
+<dependency>
+    <groupId>org.apache.activemq</groupId>
+    <artifactId>artemis-jms-server</artifactId>
+</dependency>
 ```
--- src/main/java/com/example/springjmsdemo/SpringJmsDemoApplication.java
+++ src/main/java/com/example/springjmsdemo/SpringJmsDemoApplication.java
@@ -1,12 +1,30 @@
 package com.example.springjmsdemo;
 
+import org.apache.activemq.artemis.core.config.Configuration;
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.embedded.EmbeddedActiveMQ;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJmsDemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        // embedded server with configuration runs, though initialisation errors are thrown
+        Configuration configuration = new ConfigurationImpl();
+
+        configuration
+                .setPersistenceEnabled(false)
+                .setJournalDirectory("target/data/journal")
+                .setSecurityEnabled(false)
+                .addAcceptorConfiguration("in-vm", "vm://0")
+                .addAcceptorConfiguration("tcp", "tcp://127.0.0.1:61616");
+
+        EmbeddedActiveMQ server = new EmbeddedActiveMQ();
+        server.setConfiguration(configuration);
+        server.start();
+
         SpringApplication.run(SpringJmsDemoApplication.class, args);
     }
 
