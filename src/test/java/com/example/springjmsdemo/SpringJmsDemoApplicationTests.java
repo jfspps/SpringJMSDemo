8c6a9cd373db5d785e7f0633f8a7c5719cd42b57
jfspps <jfsapps@gmail.com>
2021-02-26 00:38:54 +0000

    first commit with embedded server

---
+++ src/test/java/com/example/springjmsdemo/SpringJmsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjmsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
